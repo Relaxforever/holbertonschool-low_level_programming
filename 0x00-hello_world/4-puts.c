0bfd30616f95ff2a5c16b39f3df07d5c77285bbf
root <869@holbertonschool.com>
2019-06-19 18:11:29 +0000

    Adding the 4 one betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char helloworld[] = "\"Programming is like building a multilingual puzzle";
+	char helloworld = "\"Programming is like building a multilingual puzzle";
 
 	puts(helloworld);
 
