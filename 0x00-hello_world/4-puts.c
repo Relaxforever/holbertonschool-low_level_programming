cdb89cddf4c0374ee28ed6ed770ae4e61265afb8
root <869@holbertonschool.com>
2019-06-19 18:08:50 +0000

    Adding the 4 one betty

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+/**
+ * main - Entry point, its a void
+ *
+ * Return: helloworld variable
+ */
+int main(void)
+{
+	char helloworld[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(helloworld);
+
+}
+
