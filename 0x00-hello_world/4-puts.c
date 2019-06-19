b2950bcac4b7ed721f9fcb9a9350d7b99736cf06
root <869@holbertonschool.com>
2019-06-19 18:14:15 +0000

    Correcting the tabs and whitespace

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,7 @@
  */
 int main(void)
 {
-	char helloworld = "\"Programming is like building a multilingual puzzle";
-
-	puts(helloworld);
-
+char helloworld[] = "\"Programming is like building a multilingual puzzle";
+puts(helloworld);
 }
 
