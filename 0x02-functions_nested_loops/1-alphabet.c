125067fd74865ea7b293226d5dde61a0ebf0eaa5
root <869@holbertonschool.com>
2019-06-24 21:28:14 +0000

    ADDING points for holberton projects

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+   
+ 
+	print_alphabet()
+	{
+		for(int c = 97; c >= 122; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+
+	}	
+}
