d297b14a8e21df2a346f16e2c22d1fd892f68fbb
root <869@holbertonschool.com>
2019-06-25 21:02:03 +0000

    adding 11 pointh and .h file

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_to_98 - check the code for Holberton School students.
+ *@n : take the number
+ *
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		int tmp;
+
+		for (tmp = n; tmp >= 98; tmp--)
+		{
+			printf("%d", tmp);
+			if (tmp != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+	if (n < 98)
+	{
+		int tmp2;
+
+		for (tmp2 = n; tmp2 <= 98; tmp2++)
+		{
+			printf("%d", tmp2);
+			if (tmp2 != 98)
+			{
+				printf(",");
+				printf(" ");
+			}
+		}
+	}
+	else
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+}
