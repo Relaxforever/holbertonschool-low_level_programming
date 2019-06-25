7cef9e25310705e2d724e1d5d769df4d477ae9ad
root <869@holbertonschool.com>
2019-06-25 19:41:45 +0000

    Deleting the putchar unnecesary function

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *add - check the code for Holberton School students.
- * @a - gets the first number
- * @b - gets the second number
+ * add - check the code for Holberton School students.
+ * @a : gets the first number
+ * @b : gets the second number
  * Return: Always 0.
  */
 int add(int a, int b)
