c46d8212db3cc48bce4854366b4b7de6ff0ac5f0
root <869@holbertonschool.com>
2019-06-27 02:10:40 +0000

    Correcting fibonacci secuence

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,18 @@
 #include <stdio.h>
 int main (void)
 {
-	long int fibbo;
-	long int nacci = 1;
-	long int next;
-	int varfor;
-	for (varfor = 0; varfor <= 50; varfor++)
+	unsigned long long int i;
+	unsigned long long int fibo = 0;
+	unsigned long long int nacci = 1;
+	unsigned long long int nextTerm;
+
+	for (i = 1; i < 52; ++i)
 	{
-		if (varfor <= 1)
-		{
-			next = varfor;
-		}
-		else
-		{
-			next = fibbo + nacci;
-			fibbo = nacci;
-			nacci = next;
-		}
-		printf("%ld",next );
-		printf("\n");
-		
+		printf("%lld, ", fibo);
+		nextTerm = fibo + nacci;
+		fibo = nacci;
+		nacci = nextTerm;
 	}
+	printf("\n");
+    return 0;
 }
