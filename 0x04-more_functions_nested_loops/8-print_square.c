997bd0805f9046534316d87e8da6c4f4c95ee501
root <869@holbertonschool.com>
2019-06-27 21:35:57 +0000

    adding from 8 to 9

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+* print_square - print the numbers from 0 to 9
+*@size : takes the integer given
+*/
+void print_square(int size)
+{
+	int tmp;
+	int tmp2;
+	int space = 32;
+	int shared = 35;
+
+	if (size > 0)
+	{
+		for (tmp = 0; tmp < size; tmp++)
+		{
+			_putchar(space);
+			_putchar('\n');
+			for (tmp2 = 0; tmp2 < size; tmp2++)
+			{
+				_putchar(shared);
+			}
+		}
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
