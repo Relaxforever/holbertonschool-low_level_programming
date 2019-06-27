3bb5f0d008faa294160e468b01fbcbef8d1ed658
root <869@holbertonschool.com>
2019-06-27 21:52:23 +0000

    removing space variable

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,14 +7,12 @@ void print_square(int size)
 {
 	int tmp;
 	int tmp2;
-	int space = 32;
 	int shared = 35;
 
 	if (size > 0)
 	{
 		for (tmp = 0; tmp < size; tmp++)
 		{
-			_putchar(space);
 			for (tmp2 = 0; tmp2 < size; tmp2++)
 			{
 				_putchar(shared);
