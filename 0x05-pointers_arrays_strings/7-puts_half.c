2c7bcb2a9be18ea57148542ef87b683e80d57613
root <869@holbertonschool.com>
2019-07-02 20:17:51 +0000

    Adding the seventh point and updating header file

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+* _strlen - counts the len of a String
+* @s : takes the position of the string
+* Return: return the length of a  string
+*/
+void puts_half(char *str)
+{
+	int length;
+	int cont;
+
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		length = length / 2;
+		for(cont = length; *(str + cont) != '\0'; cont++)
+		{
+			_putchar(*(str + cont));
+		}
+	}
+	else
+	{
+		length = (length - 1) / 2;
+		for(cont = length; *(str + cont) != '\0'; cont++)
+		{
+			_putchar(*(str + cont));
+		}
+	}
+	_putchar('\n');
+}
