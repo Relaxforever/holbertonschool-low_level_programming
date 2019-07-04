e9dd9fae660562c8cbff9993ba5f9341cf7716d5
root <869@holbertonschool.com>
2019-07-04 20:46:44 +0000

    Making corrections of 3 and uploading the 4 point

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+/**
+* reverse_array - reverses an array
+*@a : takes the string
+*@n : takes the byte number
+*/
+void reverse_array(int *a, int n)
+{
+	int arra[1020];
+	int cont;
+	int rever;
+
+	for (cont = 0; cont < n; cont++)
+	{
+		arra[cont] = a[cont];
+	}
+	for (rever = 0; rever < n; rever++)
+	{
+		a[rever] = arra[cont];
+		cont--;
+	}
+}
