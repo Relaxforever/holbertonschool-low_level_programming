c3aba6608c80808e3ecfeb1d88876c88f2c627b8
root <869@holbertonschool.com>
2019-07-04 20:37:32 +0000

    Correcting logic mistakes with || and &&

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
-	for (len = 0; s1[len] != '\0' && s2[len] != '\0'; len++)
+	for (len = 0; s1[len] != '\0' || s2[len] != '\0'; len++)
 	{
 		if (s1[len] != s2[len])
 		{
