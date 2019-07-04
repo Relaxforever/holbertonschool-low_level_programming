8f9ed4e87dcf0c29d4ee3e0bd24a3914d850be84
root <869@holbertonschool.com>
2019-07-04 20:40:29 +0000

    Correcting if statement

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,9 +8,9 @@ int _strcmp(char *s1, char *s2)
 {
 	int len;
 
-	for (len = 0; s1[len] != '\0' && s2[len] != '\0'; len++)
+	for (len = 0; *s1 != '\0' && *s2 != '\0'; len++)
 	{
-		if (s1[len] != s2[len])
+		if (s1 != s2)
 		{
 			return (*s1 - *s2);
 		}
