b2298ef3e6bcf3f070eed108fc311d06a88e7e1a
root <869@holbertonschool.com>
2019-07-04 17:27:54 +0000

    Adding the third point

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+/**
+ * _strcmp - compare two strings and print the difference between the two
+ *@s1: get the first string
+ *@s2: get the second string
+ * Return: return the diference between the two strings
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int lenF, lenS;
+
+	for (lenF = 0; s1[lenF] != '\0'; lenF++)
+	{
+	}
+	for (lenS = 0; s2[lenS] != '\0'; lenS++)
+	{
+	}
+	return (*s1 - *s2);
+}
