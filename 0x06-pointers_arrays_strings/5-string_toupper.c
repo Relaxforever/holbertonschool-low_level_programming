01193dee3501d473c36651a3260932939da7dcaf
root <869@holbertonschool.com>
2019-07-04 21:47:11 +0000

    Adding the fifth point and header

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+/**
+*string_toupper - changes all lowercase letters of a string to uppercase
+*@*: change the words
+*Return: The Uppercase string
+*/
+char *string_toupper(char *d)
+{
+	int len;
+	int a = 97;
+	int z = 122;
+	int sub = 32;
+
+	for (len = 0; d[len] != '\0'; len++)
+	{
+		if ((d[len] > a) && (d[len] < z) )
+		{
+			d[len] = d[len] - sub;
+		}
+	}
+	return (d);
+}
