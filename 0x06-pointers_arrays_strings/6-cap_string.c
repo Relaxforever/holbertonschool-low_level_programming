31a70ceb7b23730b63d8c03d65ded77b9c6120c1
root <869@holbertonschool.com>
2019-07-04 23:17:42 +0000

    Adding the sixth point

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 /**
 *cap_string - makes capital letters after a certain conditio
-*@m: 
-*
+*@m: takes the pointer
+*Return: return the change capital letters
 */
 char *cap_string(char *m)
 {
@@ -9,24 +9,24 @@ char *cap_string(char *m)
 
 	for (len = 0; m[len] != '\0'; len++)
 	{
-		if( m[len] == 0)
+		if (m[len] == 0)
 		{
-			if( m[len] >= 97 && m[len] <= 122)
+			if (m[len] >= 97 && m[len] <= 122)
 			{
 				(m[len] = m[len] - 32);
 			}
 		}
-		if ( m[len] == ',' || m[len] == ';' || m[len] == '.' ||
+			if (m[len] == ',' || m[len] == ';' || m[len] == '.' ||
 			m[len] == '!' || m[len] == '?' || m[len] == '"' ||
 			m[len] == '(' || m[len] == ')' || m[len] == '{' ||
 			m[len] == '}' || m[len] == ' ' || m[len] == '\n' ||
-			m[len] == '\t' )
+			m[len] == '\t')
 			{
-				if ( m[len + 1] >= 97 && m[len + 1] <= 122)
-					{
-						(m[len + 1] = m[len + 1] - 32);
-					}
+				if (m[len + 1] >= 97 && m[len + 1] <= 122)
+				{
+					m[len + 1] = m[len + 1] - 32;
+				}
 			}
 	}
-	return (m);
+return (m);
 }
