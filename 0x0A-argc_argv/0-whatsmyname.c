0f8234b44a95124f68c01ff8a51f37d683033e52
root <869@holbertonschool.com>
2019-07-13 23:46:54 +0000

    Fixing checker mistakes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
-* nameofprogram - prints the name of the program
+*main - prints the name of the program
 *@argc: counts the number of arguments
 *@argv: stores the arguments in a array
 *Return: always zero
 */
 
-int nameofprogram(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s", argv[argc - argc]);
 	return (0);
