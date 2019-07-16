716c1ae0c751a40f3be1a7038b3739518ed69594
root <869@holbertonschool.com>
2019-07-16 22:55:17 +0000

    Working point

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -44,14 +44,18 @@ char *str_concat(char *s1, char *s2)
 	compl = s1len + s2len;
 
 	buff = malloc((compl + 1) * sizeof(char));
-
+	if (buff == NULL)
+	{
+		return (NULL);
+	}
 	for (cont = 0; cont < s1len; cont++)
 	{
 		buff[cont] = s1[cont];
 	}
-	for (cont2 = 0; cont2 < (s2len + 1); cont2++)
+	for (cont2 = 0; cont2 < s2len; cont2++)
 	{
 		buff[cont + cont2] = s2[cont2];
 	}
+	buff[cont + cont2 + 1] = '\0';
 	return (buff);
 }
