55b2accc468e007c4c4cbc68123e8ebe396bd66b
root <869@holbertonschool.com>
2019-07-22 23:49:43 +0000

    Uploading mistakes

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 /**
 *free_dog - free the memory space
 *@d: free everything
@@ -9,7 +9,7 @@ void free_dog(dog_t *d)
 	if (d != NULL)
 	{
 		free(d->name);
-		free(d->newOwner);
+		free(d->owner);
 		free(d);
 	}
 }
