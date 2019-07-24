c5d0b1612cb752913f7b07553b5480c8ed8d2d48
root <869@holbertonschool.com>
2019-07-24 19:20:45 +0000

    Fixing mistake adding action to the if logic

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,7 +10,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t cont;
 
-	if (array == NULL || size == 0)
+	if (array == NULL || size == 0 || action == NULL)
 	{
 		return;
 	}
