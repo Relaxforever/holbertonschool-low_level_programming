2cfa39bb8d28edc36e23bc3d000763d8f575a22d
root <869@holbertonschool.com>
2019-07-24 20:09:06 +0000

    Adding the second point and returning to my town to ask her for forgiveness

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+*int_index - search an integer and prints the location of the array
+*@size: the size of the array
+*@cmp: pointer to the functions.
+*@array: the array we will evaluate it.
+*Return: return in what part of the array[index] is the number located.
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int cont;
+	int compare;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (cont = 0; cont < size; cont++)
+	{
+	compare = cmp(array[cont]);
+		if (compare != 0)
+		{
+			return (cont);
+		}
+	}
+	return (-1);
+}
