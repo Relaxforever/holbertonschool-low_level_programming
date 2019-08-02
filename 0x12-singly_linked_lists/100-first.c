749272761759c779fdbde8abdb2faab6bc63b3ca
root <869@holbertonschool.com>
2019-08-02 04:47:02 +0000

    Uploading advanced one

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+void beforeMain(void) __attribute__ ((constructor));
 /**
 *beforeMain - Executes before the main
 *Return: Nothing
 */
+
 void beforeMain(void)
 {
 	char *strings = "You're beat! and yet, you must allow,\n";
