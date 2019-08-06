e1f25aa3f312d6ec5f1620a1af7c10054e10fcbe
root <869@holbertonschool.com>
2019-08-06 21:06:46 +0000

    Adding first part of point nine

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+*insert_nodeint_at_index - insert node at given position
+*@idx: index given
+*@n: number given to add
+*Return: the list with the index given
+*/
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *ptr;
+}
