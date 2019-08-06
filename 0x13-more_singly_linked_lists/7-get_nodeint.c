d1942da50b0a55e38b92a242f7c836e94b06f9c1
root <869@holbertonschool.com>
2019-08-06 19:58:53 +0000

    Fixing logical segmentation mistake

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,20 +9,21 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *ptr;
-	unsigned int cont;
+	unsigned int cont = 0;
 
 	if (head == NULL)
 	{
 		return (NULL);
 	}
 	ptr = head;
-	for (cont = 0; cont <= index; cont++)
+	while (ptr != NULL)
 	{
 		if (cont == index)
 		{
 			return (ptr);
 		}
 		ptr = ptr->next;
+	cont++;
 	}
 	return (NULL);
 }
