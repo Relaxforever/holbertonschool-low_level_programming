55b2abfb0e66571244fe3d823503a848dbd51913
root <869@holbertonschool.com>
2019-08-06 16:29:18 +0000

    Uploading fifth point

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+*free_listint2 - frees a list
+*@head: takes the node
+*Return: the frees list
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *ptr;
+
+	tmp = *head;
+	while (tmp)
+	{
+		ptr = tmp->next;
+		free(tmp);
+		tmp = ptr;
+	}
+	*head = NULL;
+}
