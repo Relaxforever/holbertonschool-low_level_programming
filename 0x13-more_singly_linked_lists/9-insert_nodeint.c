d612703ba78baa1ecba23e581e28696769c79980
root <869@holbertonschool.com>
2019-08-07 00:13:18 +0000

    Adding ninth point

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 /**
 *insert_nodeint_at_index - insert node at given position
+*@head: takes the node
 *@idx: index given
 *@n: number given to add
 *Return: the list with the index given
@@ -9,4 +10,37 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *ptr;
+	listint_t *tmp;
+	unsigned int cont;
+
+	ptr = malloc(sizeof(listint_t));
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ptr->n = n;
+	if (idx == 0)
+	{
+		ptr->next = *head;
+		*head = ptr;
+		return (ptr);
+	}
+	else
+	{
+		tmp = *head;
+		for (cont = 0; cont != idx - 1; cont++)
+		{
+			if (tmp->next != NULL)
+			{
+				tmp = tmp->next;
+			}
+			else
+			{
+				return (NULL);
+			}
+		}
+	}
+	ptr->next = tmp->next;
+	tmp->next = ptr;
+	return (ptr);
 }
