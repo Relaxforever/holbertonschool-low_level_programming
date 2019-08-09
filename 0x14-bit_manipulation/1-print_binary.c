91ef956829a4b1692c2d2bdba4481294df9ea733
root <869@holbertonschool.com>
2019-08-09 00:35:09 +0000

    Testing first point

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+/**
+*print_binary - change a number from decimal to binary
+*@n: the int given
+*return: always zero
+*/
+void print_binary(unsigned long int n)
+{
+	int len, binary, cont;
+	char zero = '0';
+	char one = '1';
+
+	if (n == 0)
+	{
+		write(1, &zero, 1);
+	}
+	for (len = 0; n >> len; len++)
+	{
+	}
+	for (cont = len - 1; cont >= 0; cont--)
+	{
+		binary = n >> cont;
+		if (binary & 1)
+		{
+			write(1, &one, 1);
+		}
+		else
+		{
+			write(1, &zero, 1);
+		}
+	}
+}
