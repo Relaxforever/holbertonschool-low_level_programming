84658392eb33ae133bae0876af652de1049477cf
root <869@holbertonschool.com>
2019-08-09 01:40:21 +0000

    Uploading third point

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,15 @@
+/**
+*set_bit - changes the bit in the position for 1
+*@n: takes the numberino.
+*@index: the place in why we will make the change
+*Return: the number changed
+**/
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < 64)
+	{
+		*n = *n | 1 << index;
+		return (1);
+	}
+	return (-1);
+}
