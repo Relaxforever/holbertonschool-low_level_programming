0f10fd92844d170e79a04ef6c1988d4073f6b2cf
root <869@holbertonschool.com>
2019-08-13 20:37:00 +0000

    Freeing memory in most cases and general case

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,26 +13,35 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	buff = malloc(sizeof(char) * letters);
 	if (buff == NULL)
 	{
+		free(buff);
 		return (0);
 	}
 	if (filename == NULL)
 	{
+		free(buff);
 		return (0);
 	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
+		free(buff);
 		return (0);
 	}
 	len = read(fd, buff, letters);
 	if (len == -1)
 	{
+		close(fd);
+		free(buff);
 		return (0);
 	}
 	len2 = write(STDOUT_FILENO, buff, len);
 	if (len2 == -1)
 	{
+		close(fd);
+		free(buff);
 		return (0);
 	}
+	close(fd);
+	free(buff);
 	return (len2);
 }
