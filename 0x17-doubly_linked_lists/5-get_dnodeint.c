e7383d7d6a751e57dbcd9df5ad5a2489be41d5aa
root <869@holbertonschool.com>
2019-09-14 18:27:41 +0000

    adding fifth point

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+* get_dnodeint_at_index - prints all the elements of the list.
+*@head: the list.
+*@index: the number.
+*Return: the number of nodes of 0 if none.
+*/
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int cont = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (cont = 0; cont < index; cont++)
+	{
+		head = head->next;
+	}
+	return (head);
+}
