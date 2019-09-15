a3a9ce48733ca86545cee0079b9caf3c78107a27
root <869@holbertonschool.com>
2019-09-15 16:11:02 +0000

    adding edge cases

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -50,6 +50,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		{
 			tmp = tmp->next;
 		}
+		if (idx == lenofnode)
+		{
+			add_dnodeint_end(h, n);
+			return (*h);
+		}
 		ptr->prev = tmp;
 		ptr->next = tmp->next;
 		ptr->n = n;
