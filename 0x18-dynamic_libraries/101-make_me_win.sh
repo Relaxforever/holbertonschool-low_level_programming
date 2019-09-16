be2616289033533f370f8c2d91c62d0a38bb57dd
Fidel Caicedo <869@holbertonschool.com>
2019-09-16 20:13:37 +0000

    Killer queen has already touch this button

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget https://github.com/Relaxforever/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/killerqueen.so
-export LD_PRELOAD=$PWD/killerqueen.so ./gm
+export =LD_PRELOAD=$PWD/killerqueen.so ./gm
