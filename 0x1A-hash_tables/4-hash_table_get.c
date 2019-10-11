e7f78f114346eb0bd30aedd4d81b52f1e4702adf
Fidel Caicedo <869@holbertonschool.com>
2019-10-11 22:32:31 +0000

    adding fourth point

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+#include <string.h>
+/**
+* hash_table_get - gets the function that retrieves a value
+* @ht: the hash table I want to look
+* @key: the keys I'm looking for
+* Return: the value associated
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
+	return (NULL);
+}
