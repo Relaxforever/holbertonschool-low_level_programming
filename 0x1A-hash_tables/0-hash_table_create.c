6ea364470d5e83c8c9e00c876c1c432724902957
Fidel Caicedo <869@holbertonschool.com>
2019-10-11 16:20:52 +0000

    changing from malloc to calloc to initiliaze

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,11 +11,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 	init_hash = malloc(sizeof(hash_table_t));
 	if (init_hash == NULL)
 		return (NULL);
-	init_hash->array = malloc(sizeof(hash_node_t) * size);
+	init_hash->array = calloc(sizeof(hash_node_t) * size);
 	if (init_hash == NULL)
 	{
 		free(init_hash);
 		return (NULL);
 	}
+	init_hash->size = size;
 	return (init_hash);
 }
