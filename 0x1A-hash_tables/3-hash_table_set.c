19763f771e039d05282b4e51fcafad6186f32cc6
Fidel Caicedo <869@holbertonschool.com>
2019-10-12 00:20:12 +0000

    adding third point and sixth point

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -55,6 +55,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char
 		{
 			if (strcmp(tmp->key, key) == 0)
 			{
+				free(tmp->value);
 				tmp->value = strdup(value);
 				flag = 1;
 			}
---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+/**
+* hash_table_delete - deletes a hash
+*@ht: where the hash is.
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *tmp;
+	hash_node_t *delete;
+	unsigned long  int index_array;
+
+	if (ht)
+	{
+		for (index_array = 0; index_array < ht->size; index_array++)
+		{
+			tmp = ht->array[index_array];
+			while (tmp)
+			{
+				delete = tmp;
+				free(delete->value);
+				free(delete->key);
+				free(delete);
+				tmp = tmp->next;
+			}
+			free(tmp);
+		}
+		free(ht->array);
+		free(ht);
+	}
+}
