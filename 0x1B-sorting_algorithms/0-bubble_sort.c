15c3064b1194fb6db1a56d3295964a9f62c66ae5
root <869@holbertonschool.com>
2019-10-28 02:17:08 +0000

    uploading first steps

---
+++ 0x1B-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,26 @@
+#include "sort.h"
+
+/**
+* bubble_sort - sorts the array in ascending order
+* @size: the size of the array
+* @array: the pointer of the array
+*/
+void bubble_sort(int *array, size_t size)
+{
+	size_t cont = 0, index;
+	int number;
+
+	for (cont = 0; cont < size - 1; cont++)
+	{
+		for (index = 0; index < size - cont - 1; index++)
+		{
+			if (array[index] > array[index + 1])
+			{
+				number = array[index];
+				array[index] = array[index + 1];
+				array[index + 1] = number;
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ 0x1B-sorting_algorithms/README.md
@@ -0,0 +1,1 @@
+Readme file should expand later
