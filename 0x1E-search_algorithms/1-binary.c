b35ed13bd05e4612b1270dbcbcb2a112536b9b48
Fidel Caicedo <869@holbertonschool.com>
2020-01-15 14:05:58 -0500

    uploading update binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,35 +1,50 @@
 #include <stdio.h>
 /**
-*
-*
+* array_given - prints the array given
+* @array: the array given
+* @end: the last one
+* @begin: the first one
+* Return: 1 if succesful, 0 if god forsake us
 */
-void array_given(int *array, size_t size)
+int array_given(int *array, size_t end, size_t begin)
 {
 	size_t cont;
+
 	printf("Searching in array: ");
-	for (cont = 0; cont < size; cont++)
+	for (cont = begin; cont <= end; cont++)
 	{
-		if (size + 1)
-			printf("%d, ", array[cont]);
-		else
+		if (cont == end)
 			printf("%d", array[cont]);
+		else
+			printf("%d, ", array[cont]);
 	}
 	printf("\n");
-	return;
+	return (1);
 }
+/**
+ * binary_search - applies the algorithm search algorithm
+ * @array: the array given
+ * @size: the size of the array
+ * @value: the value that will be search
+ * Return: the index of the array
+ */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t middle = 0;
+	size_t middle = 0, begin = 0, end = 0;
 
 	if (array == NULL)
 		return (-1);
-	middle = size / 2;
-	array_given(array, size);
-	if (array[middle] == value)
-		return (middle);
-	if (array[middle] > value)
-		binary_search( &array[middle], middle, value);
-	if (array[middle] < value)
-		binary_search( array, middle, value);
+	end = size - 1;
+	for ( ; begin != (end + 1); )
+	{
+		middle = (begin + end) / 2;
+		array_given(array, end, begin);
+		if (array[middle] == value)
+			return (middle);
+		if (array[middle] < value)
+			begin = middle + 1;
+		if (array[middle] > value)
+			end = middle - 1;
+	}
 	return (-1);
 }
